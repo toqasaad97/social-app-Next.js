0df4987d4d0a5c8f7203f64ee45a2dbe40945222
toqasaad97 <toqasaad1997@gmail.com>
2025-01-07 00:55:38 +0200

    uploading the image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,8 @@ import WhoToFollow from "@/components/WhoToFollow";
 import { currentUser } from "@clerk/nextjs/server";
 
 export default async function Home() {
-  const user = await currentUser();
+  const user =
+  await currentUser();
   const posts = await getPosts();
   const dbUserId = await getDbUserId();
 
--- src/app/profile/[usename]/page.tsx
+++ src/app/profile/[usename]/page.tsx
@@ -1,6 +1,40 @@
+import {
+  getProfileByUsername,
+  getUserLikedPosts,
+  getUserPosts,
+  isFollowing,
+} from "@/actions/profile.action";
+import { notFound } from "next/navigation";
+import ProfilePageClient from "./ProfilePageClient";
 
-export default function page() {
-  return (
-    <div>page</div>
-  )
+export async function generateMetadata({ params }: { params: { username: string } }) {
+  const user = await getProfileByUsername(params.username);
+  if (!user) return;
+
+  return {
+    title: `${user.name ?? user.username}`,
+    description: user.bio || `Check out ${user.username}'s profile.`,
+  };
 }
+
+async function ProfilePageServer({ params }: { params: { username: string } }) {
+  const user = await getProfileByUsername(params.username);
+
+  if (!user) notFound();
+
+  const [posts, likedPosts, isCurrentUserFollowing] = await Promise.all([
+    getUserPosts(user.id),
+    getUserLikedPosts(user.id),
+    isFollowing(user.id),
+  ]);
+
+  return (
+    <ProfilePageClient
+      user={user}
+      posts={posts}
+      likedPosts={likedPosts}
+      isFollowing={isCurrentUserFollowing}
+    />
+  );
+}
+export default ProfilePageServer;
